2b4a10156f7d2b19d0cc109aa7adedb36f407e7c
Fadi Bitar <fadibitar@mail.usf.edu>
2017-10-24 18:07:10 -0400

    argc

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <fstream>
 // #include <time.h>
 // #include <ctime>
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
+using namespace std;
 
 int main(int argc, char *argv[]){
+	if(argc != 3)
+		cout << "File input error" << endl;
+	else{
 
+	}
 	return 0;
 }
