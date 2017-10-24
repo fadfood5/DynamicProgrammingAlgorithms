ec94494f7b563345eec7ef98ef68436376615fb7
Fadi Bitar <fadibitar@mail.usf.edu>
2017-10-24 18:45:20 -0400

    added file parsing

---
+++ RobotStack.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+class RobotStack{
+	public:
+		char b;
+		char n;
+		char k;
+		void printValues(){
+			cout << "b: " << b << " n: " << n << " k: " << k << endl;
+		}
+};
--- main.cpp
+++ main.cpp
@@ -4,14 +4,47 @@
 // #include <ctime>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 #include <cstring>
+#include "RobotStack.cpp"
 using namespace std;
 
 int main(int argc, char *argv[]){
+	char x;
 	if(argc != 3)
 		cout << "File input error" << endl;
 	else{
-
+		ifstream file;
+		file.open(argv[1]);
+		if (!file) {
+		    cerr << "Unable to open file.";
+		    exit(1);   // call system to stop
+		}else{
+		    string str;
+		    bool first = true;
+		    int n;
+		    while (getline(file, str)){
+		    	if(first){
+		    		n = stoi(str);
+		    		first = false;
+		    	}else{
+		    		char temp[3];
+		    		int index = 0;
+		    		for(int i=0; i<str.length(); i++){
+		    			if(str[i] != ',' && str[i] != ' '){
+		    				temp[index] = str[i];
+		    				index++;
+		    			}
+		    		}
+		    		RobotStack robot;
+		    		robot.b = temp[0];
+		    		robot.n = temp[1];
+		    		robot.k = temp[2];
+		    		robot.printValues();
+		    	}
+		    }
+			file.close();
+		}
 	}
 	return 0;
 }
